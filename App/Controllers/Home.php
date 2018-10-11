120d7f78256c041d6f250bde7c5e1e1ef53c5282
cptJohnSilver <43903143+cptJohnSilver@users.noreply.github.com>
2018-10-11 22:03:53 +0900

    Add files via upload

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -3,20 +3,16 @@
 //Домашняя страница
 namespace App\Controllers;
 
+use \Core\Sessions;
 use \Core\View;
 use \App\Models\User;
 
 class Home {
 
 	public function index(){
-		if ((isset($_SESSION['loggedIn'])) && ($_SESSION['loggedIn'] == true)) {
-			if (isset($_SESSION['userName'])) {
-				$name = $_SESSION['userName'];
-				$loggedIn = true;
-			} else {
-				$name = "";
-				$loggedIn = false;
-			}
+		if (Sessions::getSession("loggedIn")) {
+			$name = Sessions::getSession("userName");
+			$loggedIn = true;
 			View::render("indexHome.php", ["title" => "Домашняя страница", "loggedIn" => $loggedIn, "user" => $name]);
 		} else {
 			$name = "";
--- App/Controllers/Users.php
+++ App/Controllers/Users.php
@@ -4,32 +4,34 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \Core\Sessions;
 use \App\Models\User;
 
 class Users {
 	
 	//Авторизация в системе
 	public function login(){
-		if ((isset($_REQUEST['userName'])) && (isset($_REQUEST['userPwd']))){
-			$userName = $_REQUEST['userName'];
+
+		if ((isset($_REQUEST['userLogin'])) && (isset($_REQUEST['userPwd']))){
+			$userLogin = $_REQUEST['userLogin'];
 			$userPwd = $_REQUEST['userPwd'];
-			settype($userName, "string");
+			settype($userLogin, "string");
 			settype($userPwd, "string");
-			$userLogin = User::login($userName, $userPwd);
-			if ($userLogin) {
-				$_SESSION['userLogin'] = $_REQUEST['userName'];
-				$_SESSION['userName'] = $userLogin[0]['name'];
-				$_SESSION['userId'] = $userLogin[0]['id'];
+			$userAuthorization = User::login($userLogin, $userPwd);
+			if ($userAuthorization) {
+				Sessions::setSession("userId", $userAuthorization[0]['id']);
+				Sessions::setSession("userLogin", $userLogin);
+				Sessions::setSession("userPwd", User::pwdHash($userPwd));
+				Sessions::setSession("userName", $userAuthorization[0]['name']);
+				Sessions::setSession("loggedIn", true);
 				//Для обеспечения более безопасного хранения пароля записываем его в сессию в уже хэшированном виде
-				$_SESSION['userPwd'] = User::pwdHash($_REQUEST['userPwd']);
-				$_SESSION['loggedIn'] = true;
 				$result = true;
 			} else {
 				throw new \Exception("Не удалось войти в систему.");
 				$result = false;
 			}
 		} else {
-			if ((isset($_SESSION['loggedIn'])) && ($_SESSION['loggedIn'] == true)){
+			if (Sessions::getSession("loggedIn")){
 				$result = true;
 			} else {
 				$result = false;
@@ -39,8 +41,8 @@ class Users {
 		if(!$result){
 			View::render("loginForm.php", ["title" => "Вход в систему"]);
 		} else {
-			$userBalance = User::getBalance($_SESSION['userId']);
-			View::render("lkPage.php", ["title" => "Личный кабинет", "loggedIn" => $_SESSION['loggedIn'], "user" => $_SESSION['userName'], "balance" => $userBalance[0]['balance']]);
+			$userBalance = User::getBalance(Sessions::getSession("userId"));
+			View::render("lkPage.php", ["title" => "Личный кабинет", "loggedIn" => Sessions::getSession("loggedIn"), "user" => Sessions::getSession("userName"), "balance" => $userBalance[0]['balance']]);
 		}
 
 	}
@@ -48,15 +50,20 @@ class Users {
 	//Проведение операции списания
 	public function transaction(){
 		//$qty = 0;
-		if(isset($_REQUEST['qty'])) {
-			$qty = $_REQUEST['qty'];
-			settype($qty, "integer");
-			$userBalance = User::getBalance($_SESSION['userId']);
-			if($userBalance) {
-				if($userBalance[0]['balance'] >= $qty){
-					$checkBalanceUpdate = User::updateBalance($_SESSION['userId'], $qty);
-					if ($checkBalanceUpdate){
-						$result = true;
+		if(isset($_REQUEST['quantity'])) {
+			$quantity = $_REQUEST['quantity'];
+			settype($quantity, "integer");
+			if ($quantity != 0) {
+				$userBalance = User::getBalance(Sessions::getSession("userId"));
+				if($userBalance) {
+					if($userBalance[0]['balance'] >= $quantity){
+						$checkBalanceUpdate = User::updateBalance(Sessions::getSession("userId"), $quantity);
+						if ($checkBalanceUpdate){
+							$result = true;
+						} else {
+							//TODO
+							$result = false;
+						}
 					} else {
 						//TODO
 						$result = false;
@@ -70,13 +77,13 @@ class Users {
 				$result = false;
 			}
 		}
-		$userBalance = User::getBalance($_SESSION['userId']);
-		View::render("lkPage.php", ["title" => "Личный кабинет", "loggedIn" => $_SESSION['loggedIn'], "user" => $_SESSION['userName'], "balance" => $userBalance[0]['balance'], "result" => $result, "qty" => $qty]);
+		$userBalance = User::getBalance(Sessions::getSession("userId"));
+		View::render("lkPage.php", ["title" => "Личный кабинет", "loggedIn" => $_SESSION['loggedIn'], "user" => $_SESSION['userName'], "balance" => $userBalance[0]['balance'], "result" => $result, "quantity" => $quantity]);
 	}
 
 	//Выход из системы
 	public function logout() {
-		$logout = User::logout($_SESSION['userId']);
+		$logout = User::logout(Sessions::getSession("userId"));
 		View::render("indexHome.php", ["title" => "Домашняя страница", "loggedIn" => false]);
 	}
 
