09dbf5e36c6dfe47554ce068426f39d1a7ecbf1c
cptJohnSilver <43903143+cptJohnSilver@users.noreply.github.com>
2018-10-17 11:30:09 +0900

    Add files via upload

--- App/Views/500.php
+++ App/Views/500.php
@@ -3,6 +3,11 @@ require 'baseTop.php';
 ?>
 		<h1>Ошибка</h1>
 		<p>К сожалению, возникла ошибка.</p>
+		<?php
+		if (isset($message)){
+			echo "<p>".$message."</p>";
+		}
+		?>
 		<p><a href='index.php'>Перейти на главную</a></p>
 <?php
 require 'baseFooter.php';
