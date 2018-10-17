3c8a6ce2c8e517cf4e520fc2f19aba08c354a799
cptJohnSilver <43903143+cptJohnSilver@users.noreply.github.com>
2018-10-17 11:29:48 +0900

    Add files via upload

--- App/Models/User.php
+++ App/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-//Модель личного кабинета
+//Модель работы с данными пользователя
 namespace App\Models;
 
 use \Core\Sessions;
@@ -9,16 +9,15 @@ use PDO;
 
 class User extends \Core\Model {
 
-	//Авторизация
-	public static function login($user, $userPwd) {
-		settype($user, "string");
+	//Авторизация пользователя в системе
+	public static function login($userLogin, $userPwd) {
+		settype($userLogin, "string");
 		settype($userPwd, "string");
-		//$pwd = sha1($pwd.configuration::PWD_SALT);
 		$userPwd = self::pwdHash($userPwd);
 		try {
 			$db = static::dbConnect();
 			$query = $db->prepare("SELECT id, login, name FROM users WHERE login = :user AND password = :pwd");
-			$query->bindParam(":user", $user);
+			$query->bindParam(":user", $userLogin);
 			$query->bindParam(":pwd", $userPwd);
 			$query->execute();
 			if($query->rowCount() == 1) {
@@ -28,7 +27,6 @@ class User extends \Core\Model {
 				return false;
 			}
 		} catch (PDOException $e) {
-			//TODO
 			echo $e->getMessage();
 		}
 	}
@@ -36,18 +34,14 @@ class User extends \Core\Model {
 	//Получение данных о состоянии счета
 	public static function getBalance ($userId) {
 		settype($userId, "integer");
-		//$userPwd = sha1($_SESSION['userPwd'].configuration::PWD_SALT);
-		$userPwd = Sessions::getSession("userPwd");
 		try {
 			$db = static::dbConnect();
-			$query = $db->prepare("SELECT balance FROM users WHERE id = :user AND password = :pwd");
-			$query->bindParam(":user", $userId);
-			$query->bindParam(":pwd", $userPwd);
+			$query = $db->prepare("SELECT balance FROM users WHERE id = :userId");
+			$query->bindParam(":userId", $userId);
 			$query->execute();
 			$result = $query->fetchAll(PDO::FETCH_ASSOC);
 			return $result;
 		} catch (PDOException $e) {
-			//TODO
 			echo $e->getMessage();
 		}
 	}
@@ -56,22 +50,20 @@ class User extends \Core\Model {
 	public static function updateBalance ($userId, $quantity) {
 		settype($userId, "integer");
 		settype($quantity, "integer");
-		//$userPwd = sha1($_SESSION['userPwd'].configuration::PWD_SALT);
-		$userPwd = Sessions::getSession("userPwd");
 		try {
 			$db = static::dbConnect();
 			$db->beginTransaction();
-			$query = $db->prepare("SELECT balance FROM users WHERE id = :userId AND password = :userPwd FOR UPDATE");
+			$query = $db->prepare("SELECT balance FROM users WHERE id = :userId FOR UPDATE");
 			$query->bindParam(":userId", $userId);
-			$query->bindParam(":userPwd", $userPwd);
 			$query->execute();
 			$currentBalance = $query->fetchAll(PDO::FETCH_ASSOC);
+
+			//Удостоверяемся, что баланс пользователя достаточный для списания
 			if ($currentBalance[0]['balance'] >= $quantity) {
 				$newUserBalance = $currentBalance[0]['balance'] - $quantity;
-				$query = $db->prepare("UPDATE users SET balance = :newUserBalance WHERE id = :userId AND password = :userPwd");
+				$query = $db->prepare("UPDATE users SET balance = :newUserBalance WHERE id = :userId");
 				$query->bindParam(":newUserBalance", $newUserBalance);
 				$query->bindParam(":userId", $userId);
-				$query->bindParam(":userPwd", $userPwd);
 				$query->execute();
 				$db->commit();
 				if ($query){
@@ -94,11 +86,8 @@ class User extends \Core\Model {
 	}
 
 	//Выход из системы
-	public static function logout($userId){
-		//TODO
-		settype($userId, "integer");
+	public static function logout(){
 		Sessions::destroySession();
-		return true;
 	}
 
 }
